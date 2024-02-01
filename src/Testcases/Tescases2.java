d3c3d9497d9a28036dfbe59d9a0840c42ea388f5
Naveen <Naveen@DESKTOP-7DEBHQI>
2024-02-01 17:15:49 +0530

    Hello World2

---
+++ src/Testcases/Tescases2.java
@@ -0,0 +1,12 @@
+package Testcases;
+
+public class Tescases2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World2");
+		// TODO Auto-generated method stub
+
+	}
+
+}
